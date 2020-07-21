01790a50862d1c0256aa3bd60c8382070a4fd028
Yousef <y89@outlook.com>
2020-07-21 13:35:45 +0300

    Test
    Test

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+$client = new UnifonicNextGenLib\UnifonicNextGenClient("y.abufayed@faheemapp.com", "aY3s@a7H@sWGWBQ");
+
+$client->getRest()->createSendMessage('qRgbphmlfmZ_mZ8GkRqzKVrmIlWZsX',
+    '',
+    'Test 2451',
+    '966553581389');
+
+
+
+    //Note instanse -> qRgbphmlfmZ_mZ8GkRqzKVrmIlWZsX
